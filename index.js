76632f58e350b1404bffbe910ccae94b3b9a4be4
Saiganity1 <sicatmichaeldave0411@gmail.com>
2025-12-02 22:08:26 +0800

    fix(snack): add default export to index.js so Snack can render entry file

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 registerRootComponent(App);
+
+// Ensure Snack and other bundlers that expect a default export can render the app
+export default App;
